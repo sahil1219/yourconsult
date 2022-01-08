bf79ba57821e7366455214056c3869a67f89f7c5
sahiltradyl1207 <sahil.arora@tradyl.com>
2022-01-08 17:39:09 +0530

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
+
 // Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/yourconsulting'));
- 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/yourconsulting/index.html'));
-});
+app.use(express.static('./dist/yourconsulting'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/yourconsulting/'}),
+);
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
